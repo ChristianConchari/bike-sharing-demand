9e1e4360981bed531a8d2a34d87b15466fe1a7c4
Christian Conchari <chrisconchari@gmail.com>
2024-08-24 15:30:11 -0400

    Refactor mlflow.sql to check if database exists before creating it

--- dockerfiles/postgres/mlflow.sql
+++ dockerfiles/postgres/mlflow.sql
@@ -1,1 +1,8 @@
-CREATE DATABASE mlflow_db;
+-- Check if database exists, and create it if it doesn't
+DO $$
+BEGIN
+   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'mlflow_db') THEN
+      CREATE DATABASE mlflow_db;
+   END IF;
+END
+$$;
