372d59a81d13668cb90fbb4758973eb7040cf716
Christian Conchari <chrisconchari@gmail.com>
2024-08-25 14:56:29 -0400

    refactor: Improve feature_engineering task with logging

--- airflow/dags/etl_processing.py
+++ airflow/dags/etl_processing.py
@@ -31,12 +31,13 @@ default_args = {
 )
 def etl_processing():
     """
-    ETL process for bike sharing demand data, splitting data into training and testing datasets
+    ETL process for bike sharing demand data, extracting data from a source CSV file, 
+    transforming the data, and loading it into a destination S3 bucket as two separate CSV files,
     """
     @task.virtualenv(
-    task_id='get_original_data',
-    requirements=["ucimlrepo==0.0.7", "awswrangler==3.9.1"],
-    system_site_packages=True
+        task_id='get_original_data',
+        requirements=["ucimlrepo==0.0.7", "awswrangler==3.9.1"],
+        system_site_packages=True
     )
     def get_data() -> None:
         """
@@ -60,9 +61,9 @@ def etl_processing():
         wr.s3.to_csv(df, data_path, index=False)
         
     @task.virtualenv(
-    task_id='feature_engineering',
-    requirements=["awswrangler==3.9.1"],
-    system_site_packages=True
+        task_id='feature_engineering',
+        requirements=["awswrangler==3.9.1"],
+        system_site_packages=True
     )
     def feature_engineering() -> None:
         """
@@ -70,106 +71,143 @@ def etl_processing():
         """
         import json
         import datetime
+        import logging
         import boto3
         import botocore.exceptions
         import mlflow
-        import os
-        import sys
-
         import awswrangler as wr
         import pandas as pd
         import numpy as np
-
         from airflow.models import Variable
-        from sklearn.preprocessing import LabelEncoder
+        
+        # Initialize logging
+        logging.basicConfig(level=logging.INFO)
+        logger = logging.getLogger(__name__)
 
         def one_hot_encode(df: pd.DataFrame, one_hot_cols: list) -> pd.DataFrame:
             """
-            Applies One-Hot Encoding to specified columns of a DataFrame.
-            
+            Apply One-Hot Encoding to specified columns of a DataFrame.
+
             Parameters:
             - df (pd.DataFrame): The input DataFrame.
             - one_hot_cols (list): List of column names to apply One-Hot Encoding to.
-            
+
             Returns:
             - pd.DataFrame: The DataFrame with One-Hot Encoded columns.
             """
-            # Apply One-Hot Encoding
+            logger.info(f"Applying One-Hot Encoding to columns: {one_hot_cols}")
+            # Apply One-Hot Encoding and return modified DataFrame
             df = pd.get_dummies(df, columns=one_hot_cols, drop_first=True, dtype=int)
             return df
 
         def cyclic_encode(df: pd.DataFrame, columns: list, max_value: int = 23) -> pd.DataFrame:
             """
-            Applies cyclic encoding to specified columns of a DataFrame.
-            
+            Apply cyclic encoding to specified columns of a DataFrame.
+
             Parameters:
             - df (pd.DataFrame): The input DataFrame.
             - columns (list): List of column names to apply Cyclic Encoding to.
             - max_value (int): The maximum value the cyclic variable can take (e.g., 23 for hours).
-            
+
             Returns:
             - pd.DataFrame: The DataFrame with Cyclic Encoded columns added.
             """
+            logger.info(f"Applying Cyclic Encoding to columns: {columns} with max value {max_value}")
             for col_name in columns:
-                # Compute the sin and cos components
+                # Compute sin and cos components for cyclic encoding
                 df[f'{col_name}_sin'] = np.sin(2 * np.pi * df[col_name] / max_value)
                 df[f'{col_name}_cos'] = np.cos(2 * np.pi * df[col_name] / max_value)
-                # Drop the original column
+                # Drop the original column after encoding
                 df = df.drop(col_name, axis=1)
-                
             return df
         
-        # Set paths for the original and processed data
+        
+        # Load Dataset
+        logger.info("Loading dataset from S3")
+        # Define S3 paths for original and processed data
         data_original_path = 's3://data/raw/bike_sharing_demand.csv'
         data_processed_path = 's3://data/processed/bike_sharing_demand.csv'
         
-        # Read the original dataset from S3
-        df = wr.s3.read_csv(data_original_path)
+        try:
+            # Read the original dataset from S3
+            df = wr.s3.read_csv(data_original_path)
+            logger.info(f"Dataset loaded successfully from {data_original_path}")
+        except Exception as e:  
+            logger.error(f"Failed to load dataset from {data_original_path}: {e}")
+            raise
         
-        # Drop duplicates and null values
+        # Data Cleaning
+        logger.info("Cleaning data by removing duplicates and null values")
+        # Remove duplicates and null values from the dataset
         df = df.drop_duplicates()
         df = df.dropna()
         
-        # Normalize count column by taking the log
-        df['log_cnt'] = np.log(df['cnt'])
+        # Feature Engineering
+        logger.info("Starting feature engineering")
         
-        # Mantain only selected features
+        # Normalize the 'cnt' column by taking the log
+        df['log_cnt'] = np.log(df['cnt'])
+        logger.info("Normalized 'cnt' column by applying log transformation")
+        
+        # Select features to retain in the processed dataset
         selected_columns = ['season', 'yr', 'hr', 'holiday', 'weekday', 'workingday', 'weathersit', 'temp', 'hum', 'windspeed']
         df = df[selected_columns + ['log_cnt']]
+        logger.info(f"Selected columns for processing: {selected_columns + ['log_cnt']}")
 
-        # Define lists for tracking encoded columns
+        # Lists of columns to be encoded
         one_hot_encoded_columns = ['weathersit', 'season', 'weekday']
         cyclic_encoded_columns = ['hr']
         
+        # Copy the DataFrame to avoid modifying the original
         df_encoded = df.copy()
 
-        # Encode using one-hot encoding
+        # Apply One-Hot Encoding
         df_encoded = one_hot_encode(df_encoded, one_hot_encoded_columns)
-        # Encode using cyclic encoding
+        # Apply Cyclic Encoding
         df_encoded = cyclic_encode(df_encoded, cyclic_encoded_columns)
         
+        logger.info("Feature engineering completed")
+        
+        # Save Processed Data
+        logger.info(f"Saving processed dataset to {data_processed_path}")
         # Save the processed dataset to S3
-        wr.s3.to_csv(df_encoded, data_processed_path, index=False)
+        try:
+            wr.s3.to_csv(df_encoded, data_processed_path, index=False)
+            logger.info(f"Processed dataset saved successfully to {data_processed_path}")
+        except Exception as e:
+            logger.error(f"Failed to save processed dataset to {data_processed_path}: {e}")
+            raise
 
-        # Save information about the dataset
-        client = boto3.client('s3')
+        # Update Dataset Information
+        logger.info("Updating dataset information in S3")
+        # Initialize S3 client to manage dataset information
+        s3_client = boto3.client('s3')
         data_dict = {}
         
+        # Attempt to fetch existing dataset information
         try:
-            client.head_object(Bucket='data', Key='data_info/bike_sharing_demand_data_info.json')
-            result = client.get_object(Bucket='data', Key='data_info/bike_sharing_demand_data_info.json')
+            s3_client.head_object(Bucket='data', Key='data_info/bike_sharing_demand_data_info.json')
+            result = s3_client.get_object(Bucket='data', Key='data_info/bike_sharing_demand_data_info.json')
             text = result["Body"].read().decode()
             data_dict = json.loads(text)
+            logger.info("Existing dataset information loaded from S3")
         except botocore.exceptions.ClientError as e:
-            if e.response['Error']['Code'] != "404":
-                raise e
+            # If the object is not found, continue with an empty dictionary
+            if e.response['Error']['Code'] == "404":
+                logger.info("No existing dataset information found, initializing new info dictionary")
+            else:
+                logger.error(f"Failed to fetch dataset information: {e}")
+                raise
 
-        # Get the target column and drop it from the dataset
+        # Fetch target column from Airflow Variable
         target_col = Variable.get("target_col")
+        logger.info(f"Target column for dataset: {target_col}")
+        
+        # Prepare data information for logging
         dataset_log = df.drop(columns=target_col)
         dataset_encoded_log = df_encoded.drop(columns=target_col)
         
-        # Save information about the dataset
+        # Update data dictionary with dataset details
         data_dict['columns'] = dataset_log.columns.to_list()
         data_dict['columns_encoded'] = dataset_encoded_log.columns.to_list()
         data_dict['target_col'] = target_col
@@ -178,26 +216,34 @@ def etl_processing():
         data_dict['cyclic_encoded_columns'] = cyclic_encoded_columns
         data_dict['columns_dtypes'] = {k: str(v) for k, v in dataset_encoded_log.dtypes.to_dict().items()}
 
+        # Save the unique values of categorical columns for reference
         category_encoded_dict = {}
-        
-        # Save the categories for each categorical column
         for category in one_hot_encoded_columns + cyclic_encoded_columns:
             category_encoded_dict[category] = np.sort(dataset_log[category].unique()).tolist()
-
         data_dict['categories_values_per_categorical'] = category_encoded_dict
 
-        # Track the date and time the data was processed
+        # Add the current date and time to the data dictionary
         data_dict['date'] = datetime.datetime.today().strftime('%Y/%m/%d-%H:%M:%S')
+        
+        # Convert data dictionary to JSON string
         data_string = json.dumps(data_dict, indent=2)
 
-        # Save the data dictionary to S3
-        client.put_object(
-            Bucket='data',
-            Key='data_info/bike_sharing_demand_data_info.json',
-            Body=data_string
-        )
+        # Save updated data dictionary back to S3
+        try:
+            s3_client.put_object(
+                Bucket='data',
+                Key='data_info/bike_sharing_demand_data_info.json',
+                Body=data_string
+            )
+            logger.info("Dataset information updated successfully in S3")
+        except Exception as e:
+            logger.error(f"Failed to update dataset information in S3: {e}")
+            raise
+        
+        # Log Data to MLflow
+        logger.info("Logging data to MLflow")
 
-        # Log the data dictionary to MLflow
+        # Configure MLflow tracking server
         mlflow.set_tracking_uri('http://mlflow:5000')
         experiment = mlflow.set_experiment("Bike Sharing Demand")
 
@@ -209,15 +255,13 @@ def etl_processing():
             log_system_metrics=True
         )
         
-        # Log the complete data to MLflow
+        # Log original and processed datasets to MLflow
         mlflow_dataset = mlflow.data.from_pandas(
                             df,
                             source="https://archive.ics.uci.edu/dataset/275/bike+sharing+dataset",
                             targets=target_col,
                             name="bike_sharing_complete"
                         )
-
-        # Log the encoded data to MLflow
         mlflow_dataset_encoded = mlflow.data.from_pandas(
                                     df_encoded,
                                     source="https://archive.ics.uci.edu/dataset/275/bike+sharing+dataset",
@@ -225,12 +269,14 @@ def etl_processing():
                                     name="bike_sharing_processed_encoded"
                                 )
         
-        # Log the data to MLflow
+        # Log datasets to MLflow
         mlflow.log_input(mlflow_dataset, context="Dataset")
         mlflow.log_input(mlflow_dataset_encoded, context="Dataset")
+        logger.info("Datasets logged to MLflow successfully")
 
         # End the MLflow run
         mlflow.end_run()
+        logger.info("MLflow run ended")
 
     
     @task.virtualenv(
